6e8ef51b6930d9056c55afe47b73f4644d8e1159
Zakhar Menshikov <zm681986@mail.ru>
2022-06-12 21:05:57 +0500

    Проекная №13

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
 const { PORT = 3000 } = process.env;
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
@@ -23,6 +25,4 @@ app.patch('*', (_req, res) => {
   res.status(404).send({ message: 'Не найдено' });
 });
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
+app.listen(PORT);
